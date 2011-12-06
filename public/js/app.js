fcf011cd6ae361ab950de4de706726f33d1f70ae
Keith Bingman <keith@keithbingman.com>
2011-12-06 12:15:18 +0100

    two js states displayed

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ var socket = io.connect('http://localhost');
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
+  jQuery('#body').html('connected')
 });
 socket.on('disconnect', function () {
   jQuery('#body').html('disconnected')
